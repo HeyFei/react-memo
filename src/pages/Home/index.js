966557cffd637d7192bc96bfb96ce295dc6140a1
Fei <shajiaming@gmail.com>
2022-12-01 10:16:48 +0800

    useState

---
+++ src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {useState} from "react";
+import Edit from './components/Edit'
+import List from './components/List'
+import './index.css'
+
+const Home = () => {
+  const [data, setData] = useState([]);
+
+  return (
+    <div className="app">
+      <Edit addData={setData} />
+      <List listData={data} deleteData={setData} />
+    </div>
+  );
+};
+
+export default Home;
